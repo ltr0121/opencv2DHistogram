8d094d4e89006817d1aaee8f8f96850353e656c5
DESKTOP-KHMT9SE\qshp0 <ltr0121@naver.com>
2018-12-03 05:16:37 +0900

    2D histogram 만들기

--- hw1.py
+++ hw1.py
@@ -7,6 +7,9 @@ img = cv2.imread('model1.jpg')
 #####################################################
 # TODO
 
+hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+hist = cv2.calcHist([hsv],[0,1],None,[180,256],[0,180,0,256])
 #####################################################
 
 plt.imshow(hist, interpolation='nearest')
