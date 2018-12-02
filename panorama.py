3d16fd0b8f50a6052d944a073ca76160309323b2
DESKTOP-KHMT9SE\qshp0 <ltr0121@naver.com>
2018-12-03 05:33:49 +0900

    make stitch method 2

--- panorama.py
+++ panorama.py
@@ -23,4 +23,18 @@ class Stitcher:
         if M is None:
             return None
 
-        
+        # otherwise, apply a perspective warp to stitch the images
+        # together
+        (matches, H, status) = M
+        result = cv2.warpPerspective(imageA, H, (imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
+        result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
+
+        # check to see if the keypoint matches should be visualized
+        if showMatches:
+            vis = self.drawMatches(imageA, imageB, kpsA, kpsB, matches, status)
+
+            #return a tuple of the stitched image and the visualization
+            return(result, vis)
+
+        #return the stitched image
+        return result
