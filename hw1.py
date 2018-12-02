4baff8b6a6a522e5d5336abceda90dab3b36ff1d
DESKTOP-KHMT9SE\qshp0 <ltr0121@naver.com>
2018-12-03 05:16:01 +0900

    파일 폴더에 다시 넣음

--- hw1.py
+++ hw1.py
@@ -7,9 +7,6 @@ img = cv2.imread('model1.jpg')
 #####################################################
 # TODO
 
-hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-
-hist = cv2.calcHist([hsv],[0,1],None,[180,256],[0,180,0,256])
 #####################################################
 
 plt.imshow(hist, interpolation='nearest')
